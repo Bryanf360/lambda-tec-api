aa016bf0e491bc52238fa41455d78569b3d21538
Oscar Pillajo <oscar.edu.dev@gmail.com>
2026-01-22 13:23:25 -0500

    feat: add validation of role to create user

--- dtos/create-user.dto.ts
+++ dtos/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { regularExps } from "../config";
+import { regularExps } from '../config';
 
 export class RegisterUserDto {
     private constructor(
@@ -7,27 +7,20 @@ export class RegisterUserDto {
         public readonly email: string,
         public readonly role: 'admin' | 'technician',
         public readonly password: string,
-        public readonly status: 'active' | 'inactive',
+        public readonly status: 'active' | 'inactive'
     ) {}
 
-    static create(props: {[key: string]: any}): [string?, RegisterUserDto?] {
+    static create(props: { [key: string]: any }): [string?, RegisterUserDto?] {
         const { names, lastnames, email, role, password, status } = props;
         if (!names) return ['Missing names'];
         if (!lastnames) return ['Missing lastnames'];
         if (!email) return ['Missing email'];
         if (!regularExps.email.test(email)) return ['Email is not valid'];
         if (!role) return ['Missing role'];
+        if (!['admin', 'technician'].includes(role)) return [`El rol ${role} no existe`];
         if (!password) return ['Missing password'];
         if (password.lenght < 6) return ['Password too short'];
         if (!status) return ['Missing status'];
-        return [undefined, new RegisterUserDto(
-            names,
-            lastnames,
-            email,
-            role,
-            password,
-            status
-        )]
+        return [undefined, new RegisterUserDto(names, lastnames, email, role, password, status)];
     }
-
 }
